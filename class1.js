c95523842948825857e410f7e7c1f4054fe044f2
HR-134 <161519787+HR-134@users.noreply.github.com>
2024-02-29 00:51:29 +0600

    Assignment 1

---
+++ class1.js
@@ -0,0 +1,27 @@
+// 1
+var firstname = "Syeda"
+var lastname = " Humaira"
+var fullname = firstname + lastname
+console.log(fullname)
+
+// 2
+var Num1 = 59
+var txt1 = "hello JS"
+console.log(Num1 == txt1)
+
+// 3
+var a = 5
+var b = "60"
+var c = a+ Number(b)
+console.log(c)
+ 
+// 4
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
+
+// 5
+var Num2 = 344
+var txtNum = "344"
+console.log(Num2 === txtNum)
